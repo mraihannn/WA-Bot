ebd639cfcf73c0ac088206d9460a7d512ef1cc30
mraihannn <mraihann0@gmail.com>
2023-01-11 17:39:52 +0700

    add DockerFile

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    API_KEY_RM_BG: "YOUR_API_KEY",
-    API_KEY_OPEN_AI: "YOUR_API_KEY"
+    API_KEY_RM_BG: "nKT7Y3jUDv4ASe1uehPYQkRn",
+    API_KEY_OPEN_AI: "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"
 }
--- index.js
+++ index.js
@@ -1,45 +1,46 @@
-const qrcode = require('qrcode-terminal');
-const { Client, LocalAuth } = require('whatsapp-web.js');
-const { EditPhotoHandler } = require('./feature/edit_foto');
-const { ChatAIHandler } = require('./feature/chat_ai');
-
-
+const qrcode = require("qrcode-terminal");
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const { EditPhotoHandler } = require("./feature/edit_foto");
+const { ChatAIHandler } = require("./feature/chat_ai");
 
 const client = new Client({
-    authStrategy: new LocalAuth()
+  authStrategy: new LocalAuth(),
 });
 
-
-
-client.on('qr', qr => {
-    qrcode.generate(qr, { small: true });
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
 });
 
-client.on('ready', () => {
-    console.log('Client is ready!');
+client.on("ready", () => {
+  console.log("Client is ready!");
 });
 
-client.on('message', async msg => {
+client.on("message", async (msg) => {
+  const text = msg.body.toLowerCase() || "";
 
-    const text = msg.body.toLowerCase() || '';
+  //check status
+  if (text === "!ping") {
+    msg.reply("pong");
+  }
 
-    //check status
-    if (text === '!ping') {
-        msg.reply('pong');
-    }
+  //image to sticker
+  if (text === "#sticker") {
+    const media = await msg.downloadMedia();
+    client.sendMessage(msg.from, media, {
+      sendMediaAsSticker: true,
+      stickerAuthor: "Draco",
+      stickerName: "draco",
+    });
+  }
 
-    // #edit_bg/bg_color
-    if (text.includes("#edit_bg/")) {
-        await EditPhotoHandler(text, msg);
-    }
-    // #ask/question?
-    if (text.includes("#ask/")) {
-        await ChatAIHandler(text, msg);
-    }
-
+  // #edit_bg/bg_color
+  if (text.includes("#edit_bg/")) {
+    await EditPhotoHandler(text, msg);
+  }
+  // #ask/question?
+  if (text.includes("#ask/")) {
+    await ChatAIHandler(text, msg);
+  }
 });
 
 client.initialize();
-
-
-
